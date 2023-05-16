2c92ba49d84548e7e067d1320e93002a366c0d2e
Mikater <stepchik09012004@gmail.com>
2023-05-16 11:07:03 +0300

    FrontEND MainPage and about Us. Created app main

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def main_page(request):
+    return render(request, template_name='main/MainPage.html')
+
+
+def about_us(request):
+    return render(request, template_name='main/about.html')
