3e55040942f8cf3aa213fcd6ce622dc981aeef37
Mikater <stepchik09012004@gmail.com>
2023-05-17 01:39:30 +0300

    registration to autologin

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-
+from . import views
+from .views import Registration
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('registration', Registration.as_view(), name='registration'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.views import View
+
 
 # Create your views here.
+
+class Registration(View):
+    template_name = 'registration/registration.html'
+
+    def get(self, request):
+        context = {
+            'form': UserCreationForm()
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username, password=password)
+
+            login(request, user)
+            return redirect('home')
+        context = {
+            'form': form
+        }
+        return render(request, self.template_name, context)
+
