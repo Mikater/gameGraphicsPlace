f3204f5c9713583742cea0d2c333d5a217832bb7
Mikater <stepchik09012004@gmail.com>
2023-05-16 09:51:47 +0300

    to host Update

--- gameGraphicsPlace/settings.py
+++ gameGraphicsPlace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6=jw!x9o39je^py1$cyv1=ub^uh$qst+yd@l3q-w!bsg9di8qp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
