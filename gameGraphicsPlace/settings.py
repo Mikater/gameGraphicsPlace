d2b6edb8a7f0c7e7ae974fa3630c443d44cf0ec6
Mikater <stepchik09012004@gmail.com>
2023-05-18 15:52:17 +0300

    static debug

--- gameGraphicsPlace/settings.py
+++ gameGraphicsPlace/settings.py
@@ -169,5 +169,6 @@ SOCIALACCOUNT_PROVIDERS = {
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
